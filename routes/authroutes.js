a478e7e8e5d4629af26f9972dbf72fd5915adbdc
Gowtham_FSD <133188448+GowthamaViknesh@users.noreply.github.com>
2023-09-14 12:56:10 +0530

    authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -20,7 +20,7 @@ const {
 router.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'https://6502b49110f85c2b90f6686e--superlative-pithivier-4b4a80.netlify.app/',
   })
 );
 
